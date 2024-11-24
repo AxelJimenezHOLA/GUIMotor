88323c8eb7a5fb873935d4742883abba17f6dc75
AxelJimenezHOLA <axel.jimenez85@uabc.edu.mx>
2024-11-23 16:00:32 -0800

    Cerrar puerto serial cuando se cierra el programa

--- src/main/java/org/example/motorvelocidad/AplicacionMotor.java
+++ src/main/java/org/example/motorvelocidad/AplicacionMotor.java
@@ -8,14 +8,31 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AplicacionMotor extends Application {
+    private ControladorMotor controlador;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AplicacionMotor.class.getResource("motor-GUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        controlador = fxmlLoader.getController();
+
         stage.setTitle("Gráfica de motor");
         stage.setResizable(false);
         stage.setScene(scene);
+        stage.setOnCloseRequest(event -> {
+            if (controlador != null) {
+                controlador.close();
+            }
+        });
         stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if (controlador != null) {
+            controlador.close();
+        }
+        super.stop();
     }
 
     public static void main(String[] args) {
